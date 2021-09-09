4cd7a1364cb5f619636977639382aec21bd8dfa2
Daniel <2317990h@student.gla.ac.uk>
2021-09-09 21:52:29 +0100

    feat: Button Redirect
    User redirected to wikipedia based on selection of either "identified"
    or "unidentified" buttons. Possible pages are scraped using bs4 and
    their respective hyperlinks written to a csv file.
    
    Seperate csv files for "identified" or "unidentified".
    
    POST request made on button click. Appropriate csv file read in.
    Redirect takes place after random choice.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import redirect
+from app import process
+import schedule
+import time
+import csv
+import random
+
 app = Flask(__name__)
 
+
+BASE_URL = "https://en.wikipedia.org"
+ALL_KILLERS_URL = "https://en.m.wikipedia.org/wiki/List_of_serial_killers_by_country"
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
+
+@app.route("/killer", methods=["POST"])
+def killer():
+    killer_type = list(request.form.keys())[0]
+    with open("data/%s_killers.csv" % killer_type, "r") as f:
+        reader = csv.reader(f)
+        killers = list(reader)
+    return redirect(BASE_URL + random.choice(killers[0]))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
--- app/process.py
+++ app/process.py
@@ -1,60 +1,67 @@
-from os import link
-from sre_constants import error
-import numpy as np
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import random
+import csv
 
 
-def make_clickable(val):
-    name, url = val.split('#')
-    return f'<a href="{url}">{name}</a>'
+BASE_URL = "https://en.wikipedia.org"
+ALL_KILLERS_URL = "https://en.m.wikipedia.org/wiki/List_of_serial_killers_by_country"
 
-def create_table(url):
-    base_url = "https://en.wikipedia.org"
+def scrape_killer_list(url, id1, killer_type):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    table = soup.find("table", {'class':"wikitable"})
-    rows = table.find_all('tr')
+    section = soup.find("section", id=id1)
+    lis = section.find_all("li")
+    killers = [str(li.find('a')['href']) for li in lis if str(li.find('a')['href']).startswith("/wiki/")]
+    individuals = section.find_all("div", {"class": "hatnote"})
+    for individual in individuals:
+        curr = individual.find('a')['href']
+        if (str(curr).startswith("/wiki/List")):
+            print(curr)
+            killers+=scrape_single_country(BASE_URL + curr, killer_type)
 
-    links = []
-    for row in rows:
-        try:
-            links.append(base_url + row.find('a')['href'])
-        except TypeError as e:
-            pass
+    create_file(killer_type, killers)
+    return
     
-    df = pd.read_html(str(table))[0].drop(["Source"], axis=1)
-    df['Links'] = links
-    df['Name'] = df['Name'] + '#' + df['Links']
-    df({'Name': make_clickable})
-    with open("identified_killers_table.html", "w") as f:
-        f.write(df.to_html(escape=False))
+def scrape_single_country(url, killer_type):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    tables = soup.find_all("table", {'class':"wikitable"})
+    
+    if(killer_type == "identified") and (soup.find("span", id="Unidentified_serial_killers") is not None):
+        tables = tables[0:-1]
+    elif(killer_type == "unidentified") and (soup.find("span", id="Unidentified_serial_killers") is not None):
+        tables = tables[::-1]
+        tables = tables[0:1]
+    elif(killer_type == "unidentified") and (soup.find("span", id="Unidentified_serial_killers") is None):
+        return []
+    
+    
+    links = []
+    for table in tables:
+        rows = table.find_all('tr')
+        for row in rows:
+            try:
+                links.append(row.find('a')['href'])
+            except TypeError as e:
+                pass
+        return links
+
+def create_file(filename, killer_urls):
+    with open("data/" + filename + "_killers.csv", "w") as f:
+        wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+        wr.writerow(killer_urls)
         f.close()
     return
 
-def scrape_killer(url):
-    base_url = "https://en.wikipedia.org"
-    response = requests.get(base_url + url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    summary_table = soup.find("table", {'class':"infobox"})
-    try:
-        mugshot = soup.find("a", {"class": "image"})
-    except AttributeError as e:
-        pass
-    mugshot_url = mugshot.find("img")['src']
 
 
 
 
 def main():
-
-    scrape_killer(create_table("https://en.wikipedia.org/wiki/List_of_serial_killers_in_the_United_States"))
+    scrape_killer_list(ALL_KILLERS_URL, "mf-section-1", "identified")
+    scrape_killer_list(ALL_KILLERS_URL, "mf-section-2", "unidentified")
 
 if __name__ == "__main__":
     main()
 
-
-#df = pd.read_csv("../data/identified_killers.csv")
 
